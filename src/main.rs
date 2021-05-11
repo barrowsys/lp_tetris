375a779f4562be1c1fbb54ca3607ea71e7195e0a
Ezra Barrow <barrow@tilde.team>
2021-05-11 17:08:13 -0400

    relicensing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+/*
+ * --------------------
+ * THIS FILE IS LICENSED UNDER THE FOLLOWING TERMS
+ *
+ * this code may not be used for any purpose. be gay, do crime
+ *
+ * THE FOLLOWING MESSAGE IS NOT A LICENSE
+ *
+ * <barrow@tilde.team> wrote this file.
+ * by reading this text, you are reading "TRANS RIGHTS".
+ * this file and the content within it is the gay agenda.
+ * if we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer, tea, or something stronger.
+ * -Ezra Barrow
+ * --------------------
+ */
+
 use array2d::Array2D;
 use lp_tetris::{ControlEvent, Launchpad};
 use std::thread::sleep;
